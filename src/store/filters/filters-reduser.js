2323ae840dbbff8ad9d85accacc9a0f9a141aa39
Ivan Kondzerau <kondzerau.ivan@gmail.com>
2024-09-16 16:45:45 +0300

    Add filters

---
+++ src/store/filters/filters-reduser.js
@@ -0,0 +1,18 @@
+import { ADD_FILTER, REMOVE_FILTER, CLEAR_FILTER } from "./filters-actions";
+
+export const filtersReducer = (state = [], action) => {
+    switch (action.type) {
+        case ADD_FILTER: {
+            return [...state, action.filter];
+        }
+        case REMOVE_FILTER: {
+            return state.filter(e => e !== action.filter);
+        }
+        case CLEAR_FILTER: {
+            return [];
+        }
+        default: {
+            return state;
+        }
+    }
+}
--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import { positionsReducer } from "./positions/positions-reduser";
+import { filtersReducer } from "./filters/filters-reduser";
 
 
 export const rootReducer = combineReducers({
     positions: positionsReducer,
+    filters: filtersReducer
 });
