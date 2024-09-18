e6266fc14defe2f1c16fb013337ffebaa9c6cfe1
Ivan Kondzerau <kondzerau.ivan@gmail.com>
2024-09-18 23:46:12 +0300

    Add handleAddFilter to the Badge component

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,14 +1,18 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { JobPosition } from './JobPosition';
 import { selectAllPositions } from 'store/positions/positions-selectors';
+import { addFilter } from 'store/filters/filters-actions';
 
 const JobList = () => {
     const positions = useSelector(selectAllPositions);
-
+    const dispatch = useDispatch();
+    const handleAddFilter = (filter) => {
+        dispatch(addFilter(filter));
+    }
   return (
     <div className='job-list'>
       {positions.map(item => (
-        <JobPosition key={item.id} {...item} />
+        <JobPosition key={item.id} handleAddFilter={handleAddFilter} {...item} />
       ))}
     </div>
   )
--- src/store/filters/filters-reduser.js
+++ src/store/filters/filters-reduser.js
@@ -3,6 +3,9 @@ import { ADD_FILTER, REMOVE_FILTER, CLEAR_FILTER } from "./filters-actions";
 export const filtersReducer = (state = [], action) => {
     switch (action.type) {
         case ADD_FILTER: {
+            if(state.includes(action.filter)) {
+                return state;
+            }
             return [...state, action.filter];
         }
         case REMOVE_FILTER: {
